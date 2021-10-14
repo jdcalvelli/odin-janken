e23c3d6657cbcb280ecae3871a67b19320e6cd80
JD Calvelli <jdcalvelli@gmail.com>
2021-10-14 14:33:33 -0500

    replace console.logs in playJankenRound with return statements + check that it works

--- script.js
+++ script.js
@@ -23,47 +23,37 @@ function playJankenRound(playerSelection, computerSelection) {
 
   if (playerSelection.toUpperCase() == "ROCK"){
     if (computerSelection.toUpperCase() == "ROCK") {
-      console.log('you tied with the computer (rock to rock)');
+      return 'you tied with the computer (rock to rock)';
     }
     else if (computerSelection.toUpperCase() == "PAPER") {
-      console.log('the computer won (rock to paper)');
+      return 'the computer won (rock to paper)';
     }
     else {
-      console.log('you won (rock to scissors)');
+      return 'you won (rock to scissors)';
     }
   }
   else if (playerSelection.toUpperCase() == "PAPER") {
     if (computerSelection.toUpperCase() == "ROCK") {
-      console.log('you won (paper to rock)');
+      return 'you won (paper to rock)';
     }
     else if (computerSelection.toUpperCase() == "PAPER") {
-      console.log('you tied with the computer (paper to paper)');
+      return 'you tied with the computer (paper to paper)';
     }
     else {
-      console.log('you lost (paper to scissors)');
+      return 'you lost (paper to scissors)';
     }
   }
   else if (playerSelection.toUpperCase() == "SCISSORS") {
       if (computerSelection.toUpperCase() == "ROCK") {
-        console.log('you lost (scissors to rock)');
+        return 'you lost (scissors to rock)';
       }
       else if (computerSelection.toUpperCase() == "PAPER") {
-        console.log('you won (scissors to paper)');
+        return 'you won (scissors to paper)';
       }
       else {
-        console.log('you tied with the computer (scissors to scissors)');
+        return 'you tied with the computer (scissors to scissors)';
       }
   }
 }
 
-playJankenRound('rock', 'rock');
-playJankenRound('rock', 'paper');
-playJankenRound('rock', 'scissors');
-
-playJankenRound('paper', 'rock');
-playJankenRound('paper', 'paper');
-playJankenRound('paper', 'scissors');
-
-playJankenRound('scissors', 'rock');
-playJankenRound('scissors', 'paper');
-playJankenRound('scissors', 'scissors');
+console.log(playJankenRound('rock', 'rock'))
