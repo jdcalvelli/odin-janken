c1adc75e5caa26693c083dc738b22bdab8ecf562
JD Calvelli <jdcalvelli@gmail.com>
2021-10-14 14:28:35 -0500

    add playJankenRound() function and test all possible iterations

--- script.js
+++ script.js
@@ -18,3 +18,52 @@ function computerPlay() {
 }
 
 console.log(computerPlay());
+
+function playJankenRound(playerSelection, computerSelection) {
+
+  if (playerSelection.toUpperCase() == "ROCK"){
+    if (computerSelection.toUpperCase() == "ROCK") {
+      console.log('you tied with the computer (rock to rock)');
+    }
+    else if (computerSelection.toUpperCase() == "PAPER") {
+      console.log('the computer won (rock to paper)');
+    }
+    else {
+      console.log('you won (rock to scissors)');
+    }
+  }
+  else if (playerSelection.toUpperCase() == "PAPER") {
+    if (computerSelection.toUpperCase() == "ROCK") {
+      console.log('you won (paper to rock)');
+    }
+    else if (computerSelection.toUpperCase() == "PAPER") {
+      console.log('you tied with the computer (paper to paper)');
+    }
+    else {
+      console.log('you lost (paper to scissors)');
+    }
+  }
+  else if (playerSelection.toUpperCase() == "SCISSORS") {
+      if (computerSelection.toUpperCase() == "ROCK") {
+        console.log('you lost (scissors to rock)');
+      }
+      else if (computerSelection.toUpperCase() == "PAPER") {
+        console.log('you won (scissors to paper)');
+      }
+      else {
+        console.log('you tied with the computer (scissors to scissors)');
+      }
+  }
+}
+
+playJankenRound('rock', 'rock');
+playJankenRound('rock', 'paper');
+playJankenRound('rock', 'scissors');
+
+playJankenRound('paper', 'rock');
+playJankenRound('paper', 'paper');
+playJankenRound('paper', 'scissors');
+
+playJankenRound('scissors', 'rock');
+playJankenRound('scissors', 'paper');
+playJankenRound('scissors', 'scissors');
