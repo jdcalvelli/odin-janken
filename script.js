c1a6f8c197c68bc10511a4ff3b0128615aa49a69
JD Calvelli <jdcalvelli@gmail.com>
2021-10-14 14:03:03 -0500

    add computerPlay() to determine computer janken choice

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 //external javascript file for odin-janken
 
 console.log('test');
+
+function computerPlay() {
+  let randNum = Math.floor(Math.random() * 100);
+  console.log(randNum);
+
+  if (randNum <= 33) {
+    return computerSelection = 'rock';
+  }
+  else if (33 < randNum <= 66) {
+    return computerSelection = 'paper';
+  }
+  else {
+    return computerSelection = 'scissors'
+  }
+}
+
+console.log(computerPlay());
